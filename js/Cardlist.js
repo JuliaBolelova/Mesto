707e087729b302f1b3bafbcd174f4ab60affb5b3
JuliaBolelova <52495622+JuliaBolelova@users.noreply.github.com>
2019-12-06 17:08:01 +0300

    скорректировала классы

---
+++ js/Cardlist.js
@@ -0,0 +1,18 @@
+//Контейнер для карточек
+class CardList {
+    constructor(placesContainer, cardTemplate) {
+        this.cardTemplate = cardTemplate;
+        this.placesContainer = placesContainer;
+    }
+
+    addCard(name, link) {
+        const card = this.cardTemplate.create(name, link);
+        this.placesContainer.insertAdjacentHTML('beforeend', card);
+    }
+
+    render(cards) {
+        cards.forEach(card => {
+            this.addCard(card.name, card.link);
+        });
+    }
+}
